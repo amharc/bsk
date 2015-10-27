f78e20d51a91773f738af6af63bc5417de25a305
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2015-10-27 12:12:12 +0100

    pamy

--- bsk.c
+++ bsk.c
@@ -15,17 +15,21 @@ int main() {
     pam_handle_t *pamh;
     int r = pam_start(BSK_SERVICE_NAME, NULL, &conv, &pamh);
     if(r != PAM_SUCCESS)
-        fail(WITHOUT_ERRNO, "Unable to start PAM: %d", r);
+        fail(WITHOUT_ERRNO, "Unable to start PAM: %d (%s)", r, pam_strerror(pamh, r));
     if(!pamh)
-        fail(WITHOUT_ERRNO, "Unable to start PAM");
+        fail(WITHOUT_ERRNO, "Unable to start PAM: %d (%s)", r, pam_strerror(pamh, r));
 
     r = pam_set_item(pamh, PAM_USER_PROMPT, BSK_USER_PROMPT);
     if(r != PAM_SUCCESS)
-        fail(WITHOUT_ERRNO, "Unable to set PAM config: %d", r);
+        fail(WITHOUT_ERRNO, "Unable to set PAM config: %d (%s)", r, pam_strerror(pamh, r));
 
     r = pam_authenticate(pamh, 0);
     if(r != PAM_SUCCESS)
-        fail(WITHOUT_ERRNO, "Unable to authenticaete: %d", r);
+        fail(WITHOUT_ERRNO, "Unable to authenticaete: %d (%s)", r, pam_strerror(pamh, r));
+
+    r = pam_acct_mgmt(pamh, PAM_SILENT | PAM_DISALLOW_NULL_AUTHTOK);
+    if(r != PAM_SUCCESS)
+        fail(WITHOUT_ERRNO, "Access denied: %d (%s)", r, pam_strerror(pamh, r));
 
     run(stdin, stdout);
 
